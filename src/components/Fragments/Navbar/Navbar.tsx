dc0d9f851294b8016d88d4725191565875415eea
Ansh Goyal <anshgoyal1704@gmail.com>
2023-04-29 18:38:53 +0530

    change navbar margin

--- src/components/Fragments/Navbar/Navbar.tsx
+++ src/components/Fragments/Navbar/Navbar.tsx
@@ -120,7 +120,7 @@ export function Navbar({ links }: HeaderResponsiveProps) {
   ));
 
   return (
-    <Header height={HEADER_HEIGHT} mb={120} className={classes.root}>
+    <Header height={HEADER_HEIGHT} mb={40} className={classes.root}>
       <Container className={classes.header}>
         {/* <MantineLogo size={28} /> */}
         <Group spacing={5} className={classes.links}>
